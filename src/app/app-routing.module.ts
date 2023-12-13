c640834de9ff9b683357ae6dbc46f696fe4821aa
hectorib2001 <hectorichaso@upb.edu>
2023-12-13 00:53:02 -0400

    routing y log in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { GeneralComponent } from './general/general.component';
+import { HeaderComponent } from './header/header.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { ServicesComponent } from './services/services.component';
 
+const routes: Routes = [
+  { path: 'about', component: AboutComponent},
+  { path: 'footer', component: FooterComponent},
+  { path: 'general', component: GeneralComponent},
+  { path: 'header', component: HeaderComponent},
+  { path: 'log-in', component: LogInComponent},
+  { path: 'services', component: ServicesComponent},
+  { path: '', redirectTo: '/log-in', pathMatch: 'full'},
+]
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { HeaderComponent } from './header/header.component';
+import { GeneralComponent } from './general/general.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    HeaderComponent,
+    GeneralComponent,
+    ServicesComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
