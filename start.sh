9cd461dd379d4702a9097d13c1adb7f74ae0ce46
rriver <riverocornejor@gmail.com>
2023-12-14 18:14:41 -0400

    devopps in docker containers

---
+++ start.sh
@@ -0,0 +1,19 @@
+docker stop $(docker ps -q)
+docker rm $(docker ps -a -q)
+\rm -rf facturas_angular
+
+git clone https://github.com/riveroriv/facturas_angular
+
+# cp Dockerfile* facturas_angular/
+
+cd facturas_angular
+
+docker build --rm -t facturas-build -f Dockerfile-build .
+docker run -d facturas-build
+
+docker cp $(docker ps -a -q):/app/dist .
+
+docker build --rm -t facturas -f Dockerfile-serve .
+docker run -d -p 80:80 facturas
+
+docker ps -a
